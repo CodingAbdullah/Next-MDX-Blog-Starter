73c5817e81db39d62efa9cb63544fd6f6f14ab2d
CodingAbdullah <abdullahmd326@gmail.com>
2025-05-30 08:51:34 -0400

    MDX Components metadata, page extensions

--- mdx-medium-blog-post-provider/mdx-components.tsx
+++ mdx-medium-blog-post-provider/mdx-components.tsx
@@ -2,13 +2,25 @@ import type { MDXComponents } from 'mdx/types';
 
 // Custom MDX components for writing Medium article style blog posts
 // Optimize Links and Images using Image and Link built-in components replacing img, a tags
-// Code blocks, a, img, ul, ol, blockquote, h1, h2, h3, h4, h5, h6, custom component for handling GitHub Gists
+// Code blocks, a, b, u, i, img, ul, ol, blockquote, h1, h2, h3, h4, h5, h6, custom component for handling GitHub Gists
 // React-Syntax-Highlighting for Code Blocks (GitHub Gists)
 // Supabase containing Article Metadata (Name, Subtitle, Tags, Date, Slug, Description, etc.)
 // *Check to see if Supabase can be used to store article content*
 // Map markdown slugs to dynamic file path routing, loading static files
 // Supabase for storing article metadata (article content?)
 // AWS S3 for article images, thumbnails, etc.
+// Gray Matter for MDX content metadata
+/*
+// Example of Metadata for a MDX file
+---
+title: "Understanding SEO in MDX"
+description: "A guide on how to optimize MDX pages for search engines"
+author: "John Doe"
+date: "2025-05-28"
+tags: ["SEO", "MDX", "React"]
+slug: "/seo-in-mdx"
+---
+*/
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...components,
--- mdx-medium-blog-post-provider/next.config.ts
+++ mdx-medium-blog-post-provider/next.config.ts
@@ -1,15 +1,19 @@
 import createMDX from '@next/mdx'
  
+// Adding acceptable page extensions to be used in this application
+// Adding external domains to be used in this application
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Configure `pageExtensions` to include markdown and MDX files
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
-  // Optionally, add any other Next.js config below
+  images: {
+    domains: ["software-dot-blog-bucket.s3.us-east-2.amazonaws.com", "github.com", "kingabdullah.codes"]
+  }
 }
  
 const withMDX = createMDX({
-  // Add markdown plugins here, as desired
+  // Will add markdown plugins, if need be
 })
  
-// Merge MDX config with Next.js config
+// Wrapping app configurations with the withMDX function
 export default withMDX(nextConfig);
