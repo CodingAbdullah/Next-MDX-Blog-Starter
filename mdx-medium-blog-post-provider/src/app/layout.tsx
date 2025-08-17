183b25943a05ea042ed8bb1495750bf154b3b6ae
CodingAbdullah <abdullahmd326@gmail.com>
2025-08-17 17:52:16 -0400

    layout.tsx file refined

--- mdx-medium-blog-post-provider/src/app/layout.tsx
+++ mdx-medium-blog-post-provider/src/app/layout.tsx
@@ -1,13 +1,7 @@
-import type { Metadata } from "next";
 import "@/app/globals.css";
 import { Analytics } from "@vercel/analytics/next";
 
-// Metadata for the application
-export const metadata: Metadata = {
-  title: "MDX Medium Blog Post Provider App",
-  description: "Generated Medium style articles using this starter template",
-};
-
+// Root Layout for the simple Next.js application
 export default function RootLayout({
   children,
 }: Readonly<{
